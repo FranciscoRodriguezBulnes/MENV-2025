c7581e7cb127846feba6444016d115a778c425f3
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2025-12-09 07:44:36 +0100

    JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,78 @@
-export const register = (req, res) => {
-  res.json({ ok: "Register" });
+import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  console.log(req.body);
+  // res.json({ ok: "Register" });
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+
+    //Buscar si el usuario ya existe
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "El usuario ya está registrado" }] });
+    }
+
+    await user.save();
+
+    //JWT - JSON WEB TOKEN
+
+    // return res.json({ email, password });
+    return res.status(201).json({ ok: true });
+  } catch (error) {
+    console.log(error.code);
+    // Manejo de error de clave duplicada por mongoose
+    if (error.code === 11000) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "El usuario ya está registrado" }] });
+    } else {
+      return res.status(500).json({ errors: [{ msg: "Error del servidor" }] });
+    }
+  }
 };
 
-export const login = (req, res) => {
-  res.json({ ok: "Login" });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "Usuario o contraseña incorrectos" }] });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "Usuario o contraseña incorrectos" }] });
+    }
+
+    //Generar el JWT - JSON WEB TOKEN
+    const token = jwt.sign(
+      {
+        user: { uid: user._id },
+      },
+      process.env.JWT_SECRET,
+      // { expiresIn: "1h" }
+    );
+
+    // const payload = {
+    //   user: {
+    //     uid: user._id,
+    //   },
+    // };
+
+    return res.json({token});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ errors: [{ msg: "Error del servidor" }] });
+  }
 };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcryptjs from "bcryptjs";
 
 const userSchema = new Schema({
   email: {
@@ -6,7 +7,7 @@ const userSchema = new Schema({
     required: true,
     trim: true,
     unique: true,
-    lowoercase: true,
+    lowercase: true, // corregido el typo
     index: { unique: true },
   },
   password: {
@@ -15,4 +16,22 @@ const userSchema = new Schema({
   },
 });
 
-export const User = model("user", userSchema);
+// Encriptar la contraseña antes de guardar el usuario
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+  } catch (error) {
+    console.error("Error al encriptar la contraseña:", error);
+    throw error; // Mongoose capturará el error
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcryptjs.compare(candidatePassword, this.password);
+} ;
+
+export const User = model("User", userSchema);
+
