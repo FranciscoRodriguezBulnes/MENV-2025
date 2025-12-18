64b72cc7b03f8ba3c40df036f5bb1efa21a2cf66
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2025-12-18 18:23:38 +0100

    intento de deploy

--- index.js
+++ index.js
@@ -12,17 +12,11 @@ const app = express();
 
 const whiteList = [process.env.ORIGIN1,process.env.ORIGIN2];
 
-// app.use(
-//   cors({
-//     origin: [process.env.ORIGIN1]
-//   })
-// );
-
-
  app.use(
    cors({
     origin: function (origin, callback) {
-      if (whiteList.includes(origin)) {
+      console.log("😊😊😊 =>", origin)
+      if (!origin ||whiteList.includes(origin)) {
         return callback(null, origin);
        }
        return callback("Error de CORS: " + origin + " no autorizado");
